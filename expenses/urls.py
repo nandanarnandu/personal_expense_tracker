03535936f7d5deeace4f8da87c03fa4add2bbd52
nandanarnandu <nandanar685@gmail.com>
2025-07-21 22:32:16 +0530

    Added register,login,logout,dashboard initials

--- expenses/urls.py
+++ expenses/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from . import views # The '.' means 'from the current folder'
+from django.contrib.auth import views as auth_views # Import Django's built-in views
+from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('register/', views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='expenses/login.html'), name='login'), # The login page
+    path('logout/', auth_views.LogoutView.as_view(template_name='expenses/home.html'), name='logout'), # The logout page
+    path('dashboard/', views.dashboard, name='dashboard'), # Our new dashboard page
 ]
