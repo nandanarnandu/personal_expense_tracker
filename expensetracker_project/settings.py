f9f61f5421f0e275f32bd712166d0e451a4ca57b
nandanarnandu <nandanar685@gmail.com>
2025-08-09 17:23:46 +0530

    email sent successfull

--- expensetracker_project/settings.py
+++ expensetracker_project/settings.py
@@ -122,4 +122,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = '/dashboard/'
 LOGOUT_REDIRECT_URL = 'login' 
 LOGIN_URL = 'login'
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# Email configuration (using Gmail SMTP)
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'nanduofficial111@gmail.com'  # your Gmail address
+EMAIL_HOST_PASSWORD = 'qffi kbkn qwlr tlxu'  # your Gmail App Password
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
